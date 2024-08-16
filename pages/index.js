8a6d568a25dff092fb66f02bc9ac8d5caaef51e6
ganbaaelmer <ganbaa_elmer@yahoo.com>
2024-08-16 13:42:02 +0800

    add vercel speed insight

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,13 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
+      <SpeedInsights/>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>NEXT js surhaar oroldoj bn</p>
+        <p>NEXT js surhaar oroldoj bn!</p>
         <p>
-          (NEXT js surhaar oroldoj bn- you’ll be building a site like this in{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
