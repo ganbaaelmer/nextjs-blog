0b14adc7f39e024ec1d8eb5d568ea3b7885df982
ganbaaelmer <ganbaa_elmer@yahoo.com>
2024-08-16 13:46:36 +0800

    preview branch

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>NEXT js surhaar oroldoj bn!</p>
+        <p>NEXT js surhaar oroldoj bn!!!!!!!!!!!!!!!!!!!!!!</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
